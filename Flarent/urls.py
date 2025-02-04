e3522964e5081ca5f9df6a4a55f21909ecde0574
BossEmma <128752098+BossEmma@users.noreply.github.com>
2025-02-04 17:35:55 +0100

    Add files via upload

--- Flarent/urls.py
+++ Flarent/urls.py
@@ -20,5 +20,6 @@ from flare import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('connect/', views.connect, name='connect')   
 ]
